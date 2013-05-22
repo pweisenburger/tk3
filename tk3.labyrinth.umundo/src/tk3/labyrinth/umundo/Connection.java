0d56c07c0182fffd8da8a52b37263c21fd4a33f8
Michael Bjoern Winkler <michi.winkler@web.de>
2013-05-22 20:07:56 +0200

    MessageFactory hinzugefügt, Gott-Klasse ausgebaut

--- tk3.labyrinth.umundo/src/tk3/labyrinth/umundo/Connection.java
+++ tk3.labyrinth.umundo/src/tk3/labyrinth/umundo/Connection.java
@@ -1,19 +1,23 @@
 package tk3.labyrinth.umundo;
 
 import org.umundo.core.Greeter;
+import org.umundo.core.Message;
 import org.umundo.core.Node;
 import org.umundo.core.Publisher;
 import org.umundo.core.Receiver;
 import org.umundo.core.Subscriber;
 
-public abstract class Connection {
+public class Connection {
 	
 	private String id;
 	private Publisher publisher;
 	private Subscriber subscriber;
+	private Node node;
+	
 	
 	public Connection(Node node, String id, Greeter greeter, Receiver receiver) {
 		this.id = id;
+		this.node = node;
 		
 		publisher = new Publisher(id);
 		publisher.setGreeter(greeter);
@@ -22,6 +26,15 @@ public abstract class Connection {
 		
 		node.addPublisher(publisher);
 		node.addSubscriber(subscriber);
+	}
+	
+	public void send(Message msg) {
+		publisher.send(msg);
+	}
+	
+	public void close() {
+		node.removePublisher(publisher);
+		node.removeSubscriber(subscriber);
 	}
 
 }
--- tk3.labyrinth.umundo/src/tk3/labyrinth/umundo/God.java
+++ tk3.labyrinth.umundo/src/tk3/labyrinth/umundo/God.java
@@ -1,25 +1,54 @@
 package tk3.labyrinth.umundo;
 
+import org.umundo.core.Message;
 import org.umundo.core.Node;
 
+import tk3.labyrinth.Game;
 import tk3.labyrinth.Observer;
 import tk3.labyrinth.core.gameelements.GameElement;
 import tk3.labyrinth.core.player.Player;
+import tk3.labyrinth.core.shared.Position;
 
 public class God implements Observer {
 	
+	public final static String PREFIX = "tk3.labyrinth.";
+	
 	private Node node;
-
-	@Override
-	public void playerMoved(Player player) {
-		// TODO Auto-generated method stub
+	private Game game;
+	
+	private Connection mainConnection;
+	private Connection gameConnection;
+	
+	
+	public God(Game game) {
+		this.game = game;
 		
+		MainGreeter mainGreeter = new MainGreeter();
+		MainReceiver mainReceiver = new MainReceiver();
+		String mainID = PREFIX + "main";
+		mainConnection = new Connection(node, mainID, mainGreeter, mainReceiver);
+		
+		gameConnection = null;
+	}
+	
+	// 
+	public void movePlayer(String id, Position position) {
+		game.getPlayer(id).move(position);
+	}
+	
+	
+	// Umundo Schicht wird informiert, dass sich etwas im Spiel getan hat: 
+	@Override
+	public void elementActivated(GameElement ge) {
+		Message msg = MessageFactory.createElementActivatedMessage(game.getOwnPlayer().getId(), ge, null); //TODO: action=null!?
 	}
 
 	@Override
-	public void elementActivated(GameElement ge) {
-		// TODO Auto-generated method stub
-		
+	public void playerMoved(Player player, Position oldPosition) {
+		if(player == game.getOwnPlayer()) {
+			Message msg = MessageFactory.createPlayerPositionMessage(player.getId(), player.getPosition());
+			gameConnection.send(msg);
+		}
 	}
 	
 	
