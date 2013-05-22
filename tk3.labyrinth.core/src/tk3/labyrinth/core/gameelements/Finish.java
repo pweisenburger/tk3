ec62b67412acd489aa7aeb532da42b05c9f8bdc2
Ingo Reimund <ingo.reimund@gmail.com>
2013-05-22 11:39:34 +0200

    Ziel hinzugefügt

---
+++ tk3.labyrinth.core/src/tk3/labyrinth/core/gameelements/Finish.java
@@ -0,0 +1,10 @@
+package tk3.labyrinth.core.gameelements;
+
+public class Finish extends GameElement {
+
+	@Override
+	public boolean isTraversable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
